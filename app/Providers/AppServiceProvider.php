e1e132c7116bb91e123ac79f483526fa83fe52c0
Mark Mooibroek <mark@appnormal.com>
2015-07-02 10:49:30 +0200

    Removed incompatible constructor

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,17 +1,9 @@
 <?php namespace App\Providers;
 
-use App\Http\Middleware\CorsMiddleware;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-
-    var $corsMiddleware;
-
-    function __construct(CorsMiddleware $corsMiddleware)
-    {
-        $this->corsMiddleware = $corsMiddleware;
-    }
 
     /**
      * Register any application services.
@@ -23,10 +15,8 @@ class AppServiceProvider extends ServiceProvider
         /** @var \Illuminate\Http\Request $request */
         $request = $this->app->make('request');
 
-        if($request->isMethod('OPTIONS'))
-        {
-            $this->app->options($request->path(), function()
-            {
+        if ($request->isMethod('OPTIONS')) {
+            $this->app->options($request->path(), function () {
                 return response('OK', 200, [
                     "Access-Control-Allow-Origins" => "*",
                     "Access-Control-Allow-Headers" => "Accept, Content-Type",
